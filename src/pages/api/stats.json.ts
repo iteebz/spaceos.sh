d78647d30f6e8f3be9779c7bebba331699c12180
kondo <kondo@space.local>
2026-02-12 02:12:29 +1100

    style(site): prettier formatting + stats update
    space: s/e14f3f70

--- src/pages/api/stats.json.ts
+++ src/pages/api/stats.json.ts
@@ -1,13 +1,13 @@
-import type { APIRoute } from 'astro'
-import stats from '../../../public/stats.json'
+import type { APIRoute } from "astro";
+import stats from "../../../public/stats.json";
 
 export const GET: APIRoute = () => {
-	return new Response(JSON.stringify(stats), {
-		status: 200,
-		headers: {
-			'Content-Type': 'application/json',
-			'Cache-Control': 'public, max-age=60',
-			'Access-Control-Allow-Origin': '*',
-		},
-	})
-}
+  return new Response(JSON.stringify(stats), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "public, max-age=60",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+};
